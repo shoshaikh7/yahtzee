e7b51e4318d7b9524926485150851002b1c3d7c4
Shoaib Shaikh <shoshaikh7@gmail.com>
2015-10-15 13:56:39 -0400

    Started newGame function in js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log("loaded")
+// function getScore {
+//
+// };
+
+var rollCount = 0
+
+// Starts a new game, resetting some of the classes and score
+function newGame() {
+	$('.categories').each(function(){
+		$(this).removeClass('category-used');
+		$(this).find('span').text("0");
+	});
+	calculateAll();
+}
+
+$(document).ready(function() {
+
+  // Add class to a category that has been clicked on
+  $("div.categories").on('click', function(e) {
+    var $selected = $(e.target);
+    $selected.addClass('category-used');
+  });
+
+
+})
