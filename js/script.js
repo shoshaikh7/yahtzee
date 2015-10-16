892377d9b941675ff7de4904aed16346208fa4f1
Shoaib Shaikh <shoshaikh7@gmail.com>
2015-10-16 15:06:28 -0400

    worked a bit on checkOne function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var rollCount = 0
-
+// DICE
 // Dice array
 var dice = [
   {
@@ -36,14 +36,86 @@ var rollDice = function() {
       dice[i].value = Math.floor((Math.random() * 6) + 1);
       $('.die').eq(i).text(dice[i].value);
     };
+    console.log(diceValues());
+    getScore();
 };
 
 // Gets the value of dice
 var diceValues = function() {
 	return [dice[0].value,dice[1].value,dice[2].value,dice[3].value,dice[4].value]
-}
+};
+
+// CATEGORIES
+// Check array if there are any conditions that will make function true
+// Highlight category if conditions are met
+// Take away highlight when condition is false
+// Get score based on condition
+// Add to score to main score if category is clicked on
+// Disable category if already used in the current game
+
+// Ones
+var checkOnes = function() {
+  var used = false;
+  var counter = 0;
+  var sum = 0;
+  var points = function () {
+    for (i = 0; i < diceValues().length; i++) {
+			if(diceValues()[i] === 1) {
+        $("#ones").css("background-color", "rgba(126, 249, 0, 0.47)");
+				sum += 1;
+        counter ++;
+			} else {
+        $("#ones").css("background-color", "transparent");
+      };
+		};
+    $('#ones span').text(sum);
+  };
+  points();
+};
+
+// Twos
 
 
+// Threes
+
+
+// Fours
+
+
+// Fives
+
+
+// Sixes
+
+
+// Three of a kind
+
+
+// Four of a kind
+
+
+// Fullhouse
+
+
+// Small Straight
+
+
+// Large Straight
+
+
+// Chance
+
+
+// Yahtzee
+
+
+// Check categories
+var getScore = function () {
+  checkOnes();
+};
+
+
+// Document.ready
 $(document).ready(function() {
 
   // Roll Dice Event
