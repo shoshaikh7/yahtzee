150d98419a7771a8e7935efd5c4a3c6994c5121b
Shoaib Shaikh <shoshaikh7@gmail.com>
2015-10-18 15:13:37 -0400

    Adjusted ones category to add score to score board if clicked on

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var rollCount = 0
 var diceValues = [];
-// DICE
+
 // Dice array
 var dice = [
   {
@@ -38,7 +38,7 @@ var rollDice = function() {
       dice[i].value = Math.floor((Math.random() * 6) + 1);
       $('.die').eq(i).text(dice[i].value);
     };
-  diceValues = [dice[0].value,dice[1].value,dice[2].value,dice[3].value,dice[4].value];
+  diceValues = [dice[0].value, dice[1].value, dice[2].value, dice[3].value, dice[4].value];
   getScore();
 };
 
@@ -54,27 +54,35 @@ var rollDice = function() {
 // Disable category if already used in the current game
 
 // Ones
-var checkOnes = function() {
-  var used = false;
-  var counter = 0;
-  var sum = 0;
-  var points = function () {
-    for (i = 0; i < diceValues.length; i++) {
-			if(diceValues[i] === 1) {
-				sum += 1;
-        counter ++;
-			};
-      $('#ones span').text(sum);
-    };
-  };
-  points();
-  if (counter !== 0) {
-    $("div#ones").addClass("highlight");
-  } else {
-    $("div#ones").removeClass("highlight");
-  };
-};
+ var checkOnes = function (){
+   var counter = 0;
+   var sum = 0;
+   var used = false;
 
+   // Make used equal to true if the category has been used already
+   if ($(".categories").hasClass("category-used")) {
+     used = true;
+   };
+
+   // If the category has not been used then do all this stuff...
+   if (used === false) {
+     for (var i = 0; i < diceValues.length; i++) {
+       if (diceValues[i] === 1) {
+         sum += 1;
+         counter ++;
+         $("#ones span").text(sum);
+       } else {
+         $("#ones span").text(sum);
+       };
+     };
+     // If dice contains any "ones" than add a class which highlights it green
+     if (counter !== 0) {
+       $("div#ones").addClass("highlight");
+     } else {
+       $("div#ones").removeClass("highlight");
+     };
+   };
+ };
 
 // Twos
 
@@ -111,15 +119,29 @@ var checkOnes = function() {
 
 // Yahtzee
 
+// Adding score to score-board
+var calculateScore = function() {
+  var allPoints = 0;
+
+  $('.categories').each(function() {
+    if ($(this).hasClass('category-used') || $(this).hasClass('frozen')) {
+      allPoints += parseInt($(this).find('span').text());
+    };
+  });
+
+  $('#current h4').text(allPoints);
+};
 
 // Check categories
-var getScore = function () {
+var getScore = function() {
   checkOnes();
   // checkTwos();
   // checkThrees();
   // checkFours();
   // checkFives();
   // checkSixes();
+
+  calculateScore();
 };
 
 
@@ -133,24 +155,26 @@ $(document).ready(function() {
 
   // Hold Dice Event
   $("div.die").on('click', function(e) {
-     var $selected = $(e.target);
-     $selected.toggleClass("held");
-     for (var i = 0; i < dice.length; i++) {
-       if (dice[i].id == $selected.attr('id')) {
-         console.log("sup");
-         if (dice[i].hold == null) {
-           dice[i].hold = dice[i].value;
-         } else {
-           dice[i].hold = null;
-         };
-       };
-     };
-   });
+    var $selected = $(e.target);
+    $selected.toggleClass("held");
+    for (var i = 0; i < dice.length; i++) {
+      if (dice[i].id == $selected.attr('id')) {
+        console.log("sup");
+        if (dice[i].hold == null) {
+          dice[i].hold = dice[i].value;
+        } else {
+          dice[i].hold = null;
+        };
+      };
+    };
+  });
 
-   // Add class to a category that has been clicked on
-     $("div.categories").on('click', function(e) {
-       var $selected = $(e.target);
-       $selected.addClass('category-used');
-     });
+  // Add class to a category that has been clicked on
+  $("div.categories").on('click', function(e) {
+    var $selected = $(e.target);
+    $selected.addClass('category-used');
+  });
+
+
 
 });
